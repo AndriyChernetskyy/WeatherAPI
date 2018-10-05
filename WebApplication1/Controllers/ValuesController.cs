f638bd48126fd92a6fa42d35672bd9c3be1e09b3
ELEKS-SOFTWARE\andriy.chernetskyy <andrewusa921@gmail.com>
2018-10-05 18:27:21 +0300

    First commit

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+using System.Data.Entity;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        BookContext db = new BookContext();
+
+        // GET api/values
+        public IEnumerable<Book> GetBooks()
+        {
+            return db.Books;
+        }
+
+        // GET api/values/5
+        public Book GetBook(int id)
+        {
+            Book book = db.Books.Find(id);
+            return book;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void CreateBook([FromBody]Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+        }
+
+        
+        [HttpPut]
+        public void EditBook(int id, [FromBody]Book book)
+        {
+            if(id == book.Id)
+            {
+                db.Entry(book).State = EntityState.Modified;
+
+                db.SaveChanges();
+            }
+        }
+
+        // DELETE api/values/5
+        public void DeleteBook(int id)
+        {
+            Book book = db.Books.Find(id);
+            if(book != null)
+            {
+                db.Books.Remove(book);
+                db.SaveChanges();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/Models/BookDbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "FirstBook", Author = "FirstAuthor", Year = 2001 });
+            db.Books.Add(new Book { Name = "SecondBook", Author = "SecondAuthor", Year = 2002 });
+            db.Books.Add(new Book { Name = "ThirdBook", Author = "ThirdAuthor", Year = 2003 });
+
+            base.Seed(db);
+        }
+    }
+}
